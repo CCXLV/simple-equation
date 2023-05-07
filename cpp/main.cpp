723f045ab795dc423285284b991bb9e43be69ade
Giorgi Merebashvili <abracadabragio12@gmail.com>
2023-05-07 23:13:42 +0400

    first commit

---
+++ cpp/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int l;
+    int t;
+
+    int length;
+    int first;
+    int second;
+
+    length = 6 * l;
+    first = t;
+    second = t + 5;
+
+    l = (length / first) + (length / second);
+
+    std::cout << l;
+
+    return 0;
+}
---
+++ python/main.py
@@ -0,0 +1,3 @@
+def equation(t):
+    return (6 / t) - (6 / (t - 5)) - 1
+
